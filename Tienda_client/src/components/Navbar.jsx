2bde33404f38a15a08bcc3d2a940031de6d60ee0
matias mendoza <mendoza.matias.ezequiel.01@gmail.com>
2023-09-07 16:23:53 -0300

    modificacion del navbar

--- Tienda_client/src/components/Navbar.jsx
+++ Tienda_client/src/components/Navbar.jsx
@@ -1,12 +1,31 @@
-import Logo from "../assets/logoipsum.svg";
+
 import { NavLink } from "react-router-dom";
 import NavbarCSS from "./Navbar.module.css";
+import { useState } from "react";
 
-export const Navbar = () => (
+export const Navbar = () => {
+  
+  const [open,setOpen] = useState(false);
+  
+  const cambiarEstado = () => {
+    setOpen(!open)
+  }
+
+  const control = open ? `${NavbarCSS.nav_items}`:`${NavbarCSS.nav_items_open}`
+
+  return( 
   <div className={NavbarCSS.nav}>
-    <img src={Logo} alt="Logo" className="logo" />
-    <NavLink to="/">Home</NavLink>
-    <NavLink to="/product">Product</NavLink>
-    <NavLink to="/">Sign in</NavLink>
+    <div className={NavbarCSS.nav_logo}>CODE</div>
+    <div className={control}>
+      <NavLink className={NavbarCSS.nav_links} to="/">Home</NavLink>
+      <NavLink className={NavbarCSS.nav_links} to="/product">Product</NavLink>
+      <NavLink className={NavbarCSS.nav_links} to="/">Sign in</NavLink>
+    </div>
+    <div className={NavbarCSS.nav_toggle}>
+      <button onClick={cambiarEstado}>abrir</button>
+    </div>
   </div>
-);
+)
+};
+
+
